84423e8fb8ec30ea7ce1d3df3791f3933914d840
Joe Young <musicjoeyoung@gmail.com>
2025-03-06 16:29:20 -0500

    MDX seems to start to work...

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,8 +1,26 @@
-const Blog = () => {
-    return (
-        <div>Blog
+//import BlogTest from './blogtest.mdx';
+import BlogTest from "./blogtest2.mdx"
 
+const Blog: React.FC = () => {
+    return (
+        <div >
+            <header >
+                <h1 >test</h1>
+            </header>
+
+            <div >
+                {/*                 <BlogTest components={{
+                    MyComponent: () => (
+                        <div >
+                            <h3 >This is a custom component!</h3>
+                            <p>It was referenced directly in the MDX file.</p>
+                        </div>
+                    )
+                }} /> */}
+                <BlogTest />
+            </div>
         </div>
-    )
-}
-export default Blog
+    );
+};
+
+export default Blog;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import { Plugin } from 'vite'
 import { defineConfig } from 'vite'
+import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mdx() as unknown as Plugin
+  ],
   optimizeDeps: {
     include: [
       "pdfjs-dist/build/pdf.worker.min.js",
